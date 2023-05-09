6f64741a513e0da6bfa3ddc54c59502c09f998f8
jchereau2023 <julien.chereau2023@campus-eni.fr>
2023-05-09 15:16:01 +0200

    mise en place page home et test

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -11,7 +11,6 @@ class MainController extends AbstractController
     #[Route('/', name: 'main_home')]
     public function home(): Response
     {
-        return $this->render('main/home.html.twig', [
-        ]);
+        return $this->render('main/home.html.twig');
     }
 }
